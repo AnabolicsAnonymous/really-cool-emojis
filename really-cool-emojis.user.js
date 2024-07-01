eb8fcc5ed6c52c26ebcfcd56e1dc43e17c606c4d
frenchcutgreenbean <fartass55@protonmail.com>
2024-07-01 10:30:18 -0400

    sfa nerf 2.0

--- really-cool-emojis.user.js
+++ really-cool-emojis.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         really-cool-emojis
-// @version      6.9.1
+// @version      6.9.2
 // @namespace    https://github.com/frenchcutgreenbean/
 // @description  emojis and img for UNIT3D trackers
 // @author       dantayy
@@ -115,7 +115,7 @@
     if (sizePref === "default") return emote.default_width;
     if (sizePref === "large") return emote.default_width + 10;
     if (sizePref === "small") return emote.default_width - 10;
-    if (sizePref === "sfa") return Math.min(emote.width, 150);
+    if (sizePref === "sfa") return emote.default_width + 23;
   }
 
   let sizePref = "default";
@@ -535,13 +535,13 @@
     const sizePrefSelect = document.getElementById("sizePref");
     const savedSizePref = localStorage.getItem("sizePref");
     if (savedSizePref) {
-        sizePrefSelect.value = savedSizePref;
+      sizePrefSelect.value = savedSizePref;
     }
 
     sizePrefSelect.addEventListener("change", () => {
-        const selectedSizePref = sizePrefSelect.value;
-        localStorage.setItem("sizePref", selectedSizePref);
-        sizePref = sizePrefSelect.value;
+      const selectedSizePref = sizePrefSelect.value;
+      localStorage.setItem("sizePref", selectedSizePref);
+      sizePref = sizePrefSelect.value;
     });
   }
 
